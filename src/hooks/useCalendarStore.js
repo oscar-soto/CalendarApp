f1033daa9e73d983330c9fb2da2edb1e7926a3f3
Oscar <oscarantonio.soto1998@gmail.com>
2022-11-16 16:28:01 -0300

    Fix: create an even from backend

--- src/hooks/useCalendarStore.js
+++ src/hooks/useCalendarStore.js
@@ -1,4 +1,5 @@
 import { useDispatch, useSelector } from 'react-redux';
+import { calendarApi } from '../api';
 import {
   onAddNewEvent,
   onDeleteEvent,
@@ -9,6 +10,7 @@ import {
 export const useCalendarStore = () => {
   const dispatch = useDispatch();
   const { events, activeEvent } = useSelector((state) => state.calendar);
+  const { user } = useSelector((state) => state.auth);
 
   const setActiveEvent = (calendarEvent) => {
     dispatch(onSetActiveEvent(calendarEvent));
@@ -23,13 +25,15 @@ export const useCalendarStore = () => {
       dispatch(onUpdateEvent({ ...calendarEvent }));
     } else {
       // build
-      dispatch(onAddNewEvent({ ...calendarEvent, _id: new Date().getTime() }));
+      const { data } = await calendarApi.post('/events', calendarEvent);
+
+      dispatch(onAddNewEvent({ ...calendarEvent, id: data.event.id, user }));
     }
   };
 
-  const startDeleteEvent =  () => {
+  const startDeleteEvent = () => {
     // Todo: Llegar al backend
-     dispatch(onDeleteEvent());
+    dispatch(onDeleteEvent());
   };
 
   return {
