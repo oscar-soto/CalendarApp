4cd24a371e8653be487dab3967d24d08aa44c78b
Oscar <oscarantonio.soto1998@gmail.com>
2022-11-19 02:30:39 -0300

    Fix: add testing to FabDelete

--- tests/calendar/components/FabDelete.test.jsx
+++ tests/calendar/components/FabDelete.test.jsx
@@ -1,15 +1,51 @@
-import { Provider } from 'react-redux';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { FabDelete } from '../../../src/calendar/components/FabDelete';
-import { store } from '../../../src/store';
+import { useCalendarStore } from '../../../src/hooks';
+
+jest.mock('../../../src/hooks/useCalendarStore');
 
 describe('prueba en <FabDelete />', () => {
+  const mockStartDeleteEvent = jest.fn();
+  beforeEach(() => jest.clearAllMocks());
+
   test('Debe de mostrar el componente correctamente', () => {
-    render(
-      <Provider store={store}>
-        <FabDelete />
-      </Provider>
-    );
-    screen.debug();
+    // jest.fn().mockReturnValue
+    useCalendarStore.mockReturnValue({
+      hasEventSelected: false,
+    });
+
+    render(<FabDelete />);
+
+    const btn = screen.getByLabelText('btn-delete');
+    // console.log(btn.classList.toString())
+    expect(btn.classList).toContain('btn');
+    expect(btn.classList).toContain('btn-danger');
+    expect(btn.classList).toContain('fab-danger');
+    expect(btn.style.display).toBe('none');
+  });
+
+  test('Debe de mostrar el btn si hay un evento activo', () => {
+    useCalendarStore.mockReturnValue({
+      hasEventSelected: true,
+    });
+
+    render(<FabDelete />);
+
+    const btn = screen.getByLabelText('btn-delete');
+    expect(btn.style.display).toBe('');
+  });
+
+  test('Debe llamar startDeleteEvent si hay evento activo', () => {
+    useCalendarStore.mockReturnValue({
+      hasEventSelected: true,
+      startDeleteEvent: mockStartDeleteEvent,
+    });
+
+    render(<FabDelete />);
+
+    const btn = screen.getByLabelText('btn-delete');
+    fireEvent.click(btn);
+
+    expect(mockStartDeleteEvent).toHaveBeenCalled();
   });
 });
